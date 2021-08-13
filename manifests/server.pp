77a63d6231c1deb7cba11ef3e80681f4f81b866c
Bas van den Boom <basvandenboom@clonable.net>
2021-08-13 11:56:14 +0200

    Specify packages when pinning

---
+++ manifests/server.pp
@@ -0,0 +1,24 @@
+# @summary A short summary of the purpose of this class
+#
+# A description of what this class does
+#
+# @example
+#   include dnsdist::server
+define dnsdist::server (
+  String $cluster_name,
+  String $address,
+  Optional[String] $source,
+  Optional[Integer] $qps,
+  Optional[Integer] $weight,
+  Optional[String] $pool,
+){
+
+  $order = "20-${cluster_name}-01-${address}"
+
+  # Template uses $address, $cluster_name
+  concat::fragment { "${cluster_name}_server_${name}":
+    order   => $order,
+    target  => $dnsdist::params::config_file,
+    content => template('dnsdist/dnsdist_server.erb'),
+  }
+}
