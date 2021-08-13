dff99775a7df037a88f8e1c258fc571582f63815
Bas van den Boom <basvandenboom@clonable.net>
2021-08-13 11:42:45 +0200

    Default empty array for additional addresses

--- manifests/instance.pp
+++ manifests/instance.pp
@@ -3,7 +3,7 @@
 #
 define dnsdist::instance (
   String $address = '127.0.0.1:53',
-  Optional[Array[String]] $additional_addresses,
+  Array[String] $additional_addresses = [],
 ){
 
   $order = '10-instance';
